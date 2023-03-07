bfec03f10babb39b26cecc335fabdee25eec16ff
Dimitri Masetta s264948 <s264948@studenti.polito.it>
2023-03-07 10:32:09 +0100

    upd, minesweeper v2

--- exercises/practice/minesweeper/src/main/kotlin/Minesweeper.kt
+++ exercises/practice/minesweeper/src/main/kotlin/Minesweeper.kt
@@ -1,44 +1,35 @@
 data class MinesweeperBoard(val rows: List<String>) {
 
-    val matrix: MutableList<List<String>> = mutableListOf()
+    private val matrix: List<MutableList<Char>> = rows.map { row -> row.toMutableList() }
 
-    init{
-        for(row in rows){
-            val elements = row.split(' ')
-            matrix.add(elements)
-        }
-    }
-
-    fun countAdj(val row: Int, val col:Int): String{
-        val count: Int = 0
+    private fun countAdj(row: Int, col:Int): Char{
+        var count: Int = 0
         val lx = if(col > 0) col-1 else col
-        val rx = if(col - 1 < matrix[0].size) col+1 else col
+        val rx = if(col < matrix[0].size - 1) col+1 else col
         val up = if(row > 0) row-1 else row
-        val dw = if(row - 1 < matrix.size) row+1 else row
+        val dw = if(row < matrix.size - 1) row+1 else row
         for(i in up..dw){
             for(j in lx..rx){
-                if(matrix[i][j] == "*"){
+                if(matrix[i][j] == '*'){
                     count += 1
                 }
             }
         }
-        return count
+        return if(count == 0){
+            ' '
+        } else{
+            count.digitToChar()
+        }
     }
 
     fun withNumbers(): List<String> {
-        val result: MutableList<String> = mutableListOf()
         for(i in matrix.indices){
-            val strOut: String = ""
-            for(j in matrix.indices){
-                if(matrix[i][j] != "*"){
-                    strOut += countAdj(i, j)
-                }
-                else{
-                    strOut += "*"
+            for(j in matrix[0].indices){
+                if(matrix[i][j] != '*'){
+                    matrix[i][j] = countAdj(i, j)
                 }
             }
-            result.add(strOut)
         }
+        return matrix.map { row -> row.joinToString(separator = "") }
     }
-    return result
 }
