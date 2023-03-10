5c55af8b4a08ee081a93de392ab22f694b53f90e
Dimitri Masetta s264948 <s264948@studenti.polito.it>
2023-03-10 09:00:50 +0100

    upd, matrix and flatten-array

--- exercises/practice/flatten-array/src/main/kotlin/Flattener.kt
+++ exercises/practice/flatten-array/src/main/kotlin/Flattener.kt
@@ -1,5 +1,18 @@
 object Flattener {
+    var list: MutableList<Any> = mutableListOf()
     fun flatten(source: Collection<Any?>): List<Any> {
-        TODO("Implement the function to complete the task")
+        list = mutableListOf()
+        flattenRecusrsive(source)
+        return list
+    }
+
+    fun flattenRecusrsive(source: Collection<Any?>): Unit {
+        source.forEach { it ->
+            if (it is List<*>) {
+                flattenRecusrsive(it)
+            } else if (it != null) {
+                list.add(it)
+            }
+        }
     }
 }
--- exercises/practice/matrix/src/main/kotlin/Matrix.kt
+++ exercises/practice/matrix/src/main/kotlin/Matrix.kt
@@ -1,27 +1,10 @@
 class Matrix(matrixAsString: String) {
-    private val matrix: MutableList<List<Int>> = mutableListOf()
-
-    init{
-        val rows = matrixAsString.split('\n')
-        for(row in rows) {
-            val elements = row.split(' ')
-            val list: MutableList<Int> = mutableListOf()
-            for (element in elements) {
-                list.add(element.toInt())
-            }
-            matrix.add(list)
+    private val matrix: List<List<Int>> = matrixAsString.split('\n')
+        .map { row -> row.split(' ')
+            .map { element -> element.toInt() }
         }
-    }
 
-    fun column(colNr: Int): List<Int> {
-        val list: MutableList<Int> = mutableListOf()
-        for(row in matrix){
-            list.add(row[colNr - 1])
-        }
-        return list
-    }
+    fun column(colNr: Int): List<Int> = matrix.map { row -> row[colNr - 1]}
 
-    fun row(rowNr: Int): List<Int> {
-        return matrix[rowNr - 1]
-    }
+    fun row(rowNr: Int): List<Int> = matrix[rowNr - 1]
 }
