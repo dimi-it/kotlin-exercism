a29c683add3d80e90d695335e794ac76c8030572
Dimitri Masetta s264948 <s264948@studenti.polito.it>
2023-03-10 14:30:07 +0100

    upd, word-count

--- exercises/practice/word-count/src/main/kotlin/WordCount.kt
+++ exercises/practice/word-count/src/main/kotlin/WordCount.kt
@@ -1,6 +1,10 @@
 object WordCount {
 
     fun phrase(phrase: String): Map<String, Int> {
-        TODO("Implement this function to complete the task")
+        val map: MutableMap<String, Int> = mutableMapOf()
+        Regex("[a-z0-9]+('?[a-z0-9]+)?").findAll(phrase.lowercase())
+            .map{it.value}
+            .forEach{map[it] = (map[it]?.plus(1)) ?: 1}
+        return map
     }
 }
